b9497ef88cc557baab07d1f24e54cc02b9edd40e
uttam7127-creator <uttam7127@gmail.com>
2026-08-07 03:41:20 +0530

    Refactor Firebase imports and add connection alert
    Removed redundant import statement and added alert for successful connection.

--- firebase.js
+++ firebase.js
@@ -1,25 +1,18 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import {
-getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyBH3QSgukJzW0RAJWJ9qqsoSAApUey8cZ8",
   authDomain: "bloom-rewards-pro.firebaseapp.com",
   projectId: "bloom-rewards-pro",
   storageBucket: "bloom-rewards-pro.firebasestorage.app",
   messagingSenderId: "613316349558",
-  appId: "1:613316349558:web:eb9d3ac5bcfe4addd01957",
-  measurementId: "G-NWTSYSJDB2"
+  appId: "1:613316349558:web:eb9d3ac5bcfe4addd01957"
 };
 
-// Initialize
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
-export { db };
+alert("Firebase Connected Successfully");
 
+export { db };
